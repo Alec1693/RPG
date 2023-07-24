3302981a924ccc376f417931c1c2a26e8677faaf
Alec1693 <alecchristiansen@gmail.com>
2023-07-24 15:23:34 -0400

    update script.js

--- script.js
+++ script.js
@@ -36,6 +36,24 @@ const weapons = [
     }
 ];
 
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -54,8 +72,32 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg" as it dies. You gain experience points and find gold.' 
+    },
+    {
+        name: "lose",
+        "button text": ["Replay", "Replay", "Replay"],
+        "button functions": [restart, restart, restart],
+        text: 'You die.' 
+    },
+    {
+        name: "win",
+        "button text": ["Replay", "Replay", "Replay"],
+        "button functions": [restart, restart, restart],
+        text: 'You defeat the dragon! YOU WIN THE GAME!' 
     }
-]
+];
 
 //initialize buttons
 button1.onclick = goStore;
@@ -63,6 +105,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = "none";
     button1.innerHTML = location["button text"][0];
     button2.innerHTML = location["button text"][1];
     button3.innerHTML = location["button text"][2];
@@ -82,10 +125,6 @@ function goStore() {
 
 function goCave() {
     update(locations[2]);
-}
-
-function fightDragon() {
-    console.log("Fighting Dragon.");
 }
 
 function buyHealth() {
@@ -108,7 +147,7 @@ function buyWeapon() {
             let newWeapon = weapons[currentWeapon].name;
             text.innerHTML = "You now have a " + newWeapon + ".";
             inventory.push(newWeapon);
-            text.innerHTML += " In your inventory you have: " + inventory;
+            text.innerHTML += " In your inventory you have: " + " " + inventory;
         } else {
             text.innerHTML = "You do not have enough gold to buy a weapon.";
         }
@@ -132,9 +171,72 @@ function sellWeapon() {
 }
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
+    fighting = 1;
+    goFight();
+}
 
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterNameText.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+    text.innerHTML = "The " + monsters[fighting].name + " attacks.";
+    text.innerHTML += " You attack it with yuor " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerHTML = health;
+    monsterHealthText.innerHTML = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        fighting === 2 ? winGame() : defeatMonster();
+    }
+}
+
+
+
+function dodge() {
+    text.innerHTML = 'You dodge the attach from the ' + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerHTML = gold;
+    xpText.innerHTML = xp;
+    update(locations[4]);
+}
+
+function lose() {
+    update(locations[5]);
+}
+
+function winGame() {
+    update(location[6]);
+}
+
+function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerHTML = gold;
+    healthText.innerHTML = health;
+    xpText.innerHTML = xp;
+    goTown();
 }
